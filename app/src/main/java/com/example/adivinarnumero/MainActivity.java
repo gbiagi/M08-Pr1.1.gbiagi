805439c18baf61d91efd2af8e14e3b6ebc8cfeea
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2023-09-26 16:23:55 +0200

    Boton editado + toast

--- app/src/main/java/com/example/adivinarnumero/MainActivity.java
+++ app/src/main/java/com/example/adivinarnumero/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.adivinarnumero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button1 = findViewById(R.id.button);
-
+        button1.setText("Clickeame!");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Make a toast! Sköl!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
